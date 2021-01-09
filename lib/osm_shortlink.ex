b171b30ec1c495ca6a1254ef1b20ff9ab5f45eb1
Dan <daniel.kukula@chrysalisloyalty.com>
2021-01-09 23:30:32 +0000

    restore coordinates from link

--- lib/osm_shortlink.ex
+++ lib/osm_shortlink.ex
@@ -16,26 +16,28 @@ defmodule OsmShortlink do
   based on https://gist.github.com/mdornseif/5652824 by Maximillian Dornseif
 
 
-  iex(76)> OsmShortlink.osm_shortlink(51.5110,0.0550, 16)
+  iex> OsmShortlink.generate_link(51.5110,0.0550, 16)
   "http://osm.org/go/0EEQjEEb"
-  iex(78)> OsmShortlink.osm_shortlink(51.5110,0.0550, 9)
+  iex> OsmShortlink.generate_link(51.5110,0.0550, 9)
   "http://osm.org/go/0EEQjE--"
-  iex(79)> OsmShortlink.osm_shortlink(51.5110,0.0550, 9, true)
+  iex> OsmShortlink.generate_link(51.5110,0.0550, 9, true)
   "http://osm.org/go/0EEQjE--?m"
   """
-  def osm_shortlink(lat, lng, zoom, marker \\ false)
+  def generate_link(lat, lng, zoom, marker \\ false)
 
-  @spec osm_shortlink(number(), number(), pos_integer(), boolean()) :: binary()
-  def osm_shortlink(lat, lng, zoom, marker) when zoom >= 0 do
+  @spec generate_link(number(), number(), pos_integer(), boolean()) :: binary()
+  def generate_link(lat, lng, zoom, marker) when zoom >= 0 do
     x = trunc((lng + 180) * (1 <<< 32) / 360)
     y = trunc((lat + 90) * (1 <<< 32) / 180)
+
     <<code::64>> = interleave_bits(<<x::32>>, <<y::32>>, "")
 
     zoom_var = ceil((zoom + 8) / 3.0) - 1
-    link_code = osm_shortlink_code(code, zoom_var, [])
+
+    link_code = generate_link_code(code, zoom_var, [])
     zoom_remainder = zoom_remainder(zoom)
 
-    generate_link(link_code, zoom_remainder, marker)
+    generate_string(link_code, zoom_remainder, marker)
   end
 
   @spec interleave_bits(binary(), binary(), binary()) :: binary()
@@ -45,12 +47,12 @@ defmodule OsmShortlink do
     interleave_bits(arest, brest, <<ret::bits, a1::1, b1::1>>)
   end
 
-  @spec osm_shortlink_code(pos_integer(), integer(), list()) :: String.t()
-  defp osm_shortlink_code(_, -1, ret), do: :erlang.list_to_binary(ret)
+  @spec generate_link_code(pos_integer(), integer(), list()) :: String.t()
+  defp generate_link_code(_, -1, ret), do: :erlang.list_to_binary(ret)
 
-  defp osm_shortlink_code(code, z, ret) do
+  defp generate_link_code(code, z, ret) do
     digit = code >>> (58 - 6 * z) &&& 63
-    osm_shortlink_code(code, z - 1, [String.at(@chars, digit) | ret])
+    generate_link_code(code, z - 1, [String.at(@chars, digit) | ret])
   end
 
   @spec zoom_remainder(integer()) :: String.t()
@@ -58,8 +60,53 @@ defmodule OsmShortlink do
     :binary.part("--", 0, rem(zoom + 8, 3))
   end
 
-  @spec generate_link(String.t(), String.t(), boolean()) :: String.t()
-  defp generate_link(code, zoom, marker) do
+  @spec generate_string(String.t(), String.t(), boolean()) :: String.t()
+  defp generate_string(code, zoom, marker) do
     IO.chardata_to_string(["http://osm.org/go/", code, zoom, if(marker, do: "?m", else: "")])
+  end
+
+  @doc """
+  Restores the coordinates from link
+
+  iex> OsmShortlink.link_to_coordinates("http://osm.org/go/0EEQjE--")
+  {51.510772705078125, 0.054931640625}
+  """
+  @spec link_to_coordinates(String.t()) :: {float(), float()}
+  def link_to_coordinates("https://osm.org/go/" <> link) do
+    link_to_coordinates(link)
+  end
+
+  def link_to_coordinates("http://osm.org/go/" <> link) do
+    link_to_coordinates(link)
+  end
+
+  def link_to_coordinates(link) do
+    link
+    |> String.split("-")
+    |> hd()
+    |> String.split("", trim: true)
+    |> Enum.filter(fn char -> char != "-" end)
+    |> Enum.map(fn char -> :binary.match(@chars, char) |> elem(0) end)
+    |> Enum.reverse()
+    |> Enum.reduce(<<>>, fn six_bit_val, acc -> <<six_bit_val::6, acc::bits>> end)
+    |> restore_coords("", "")
+  end
+
+  @spec restore_coords(binary(), binary(), binary()) :: {float(), float()}
+  defp restore_coords(<<a1::1, b1::1, rest::bits>>, a, b) do
+    restore_coords(rest, <<a::bits, a1::1>>, <<b::bits, b1::1>>)
+  end
+
+  defp restore_coords("", lng, lat) do
+    if bit_size(lat) < 32 do
+      restore_coords("", <<lng::bits, 0::1>>, <<lat::bits, 0::1>>)
+    else
+      {restore_coord(lat, 1), restore_coord(lng, 2)}
+    end
+  end
+
+  @spec restore_coord(binary(), integer()) :: float()
+  def restore_coord(coord, multiplier) do
+    :binary.decode_unsigned(coord) * 180 * multiplier / (1 <<< 32) - multiplier * 90
   end
 end
--- test/osm_shortlink_test.exs
+++ test/osm_shortlink_test.exs
@@ -3,47 +3,47 @@ defmodule OsmShortlinkTest do
   doctest OsmShortlink
 
   test " adding marker true adds marker" do
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 9, true) == "http://osm.org/go/0EEQjE--?m"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 9, true) == "http://osm.org/go/0EEQjE--?m"
   end
 
   test " adding marker false is not adding marker" do
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 9, false) == "http://osm.org/go/0EEQjE--"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 9, false) == "http://osm.org/go/0EEQjE--"
   end
 
   test "lower zoom level removes zoom dashes" do
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 8, false) == "http://osm.org/go/0EEQjE-"
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 7, false) == "http://osm.org/go/0EEQj"
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 6, false) == "http://osm.org/go/0EEQj--"
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 5, false) == "http://osm.org/go/0EEQj-"
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 4, false) == "http://osm.org/go/0EEQ"
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 0, false) == "http://osm.org/go/0EE--"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 8, false) == "http://osm.org/go/0EEQjE-"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 7, false) == "http://osm.org/go/0EEQj"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 6, false) == "http://osm.org/go/0EEQj--"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 5, false) == "http://osm.org/go/0EEQj-"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 4, false) == "http://osm.org/go/0EEQ"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 0, false) == "http://osm.org/go/0EE--"
   end
 
   test "higher zoom level makes the url longer" do
-    assert OsmShortlink.osm_shortlink(51.5110, 0.0550, 16, false) == "http://osm.org/go/0EEQjEEb"
+    assert OsmShortlink.generate_link(51.5110, 0.0550, 16, false) == "http://osm.org/go/0EEQjEEb"
   end
 
   test "bounds go round" do
-    assert OsmShortlink.osm_shortlink(-90, -180, 0, false) == "http://osm.org/go/AAA--"
-    assert OsmShortlink.osm_shortlink(90, -180, 0, false) == "http://osm.org/go/AAA--"
-    assert OsmShortlink.osm_shortlink(90, 180, 0, false) == "http://osm.org/go/AAA--"
-    assert OsmShortlink.osm_shortlink(-90, 180, 0, false) == "http://osm.org/go/AAA--"
-    assert OsmShortlink.osm_shortlink(-180, -90, 16, false) == "http://osm.org/go/YAAAAAAA"
-    assert OsmShortlink.osm_shortlink(-180, 90, 16, false) == "http://osm.org/go/4AAAAAAA"
-    assert OsmShortlink.osm_shortlink(180, 90, 16, false) == "http://osm.org/go/4AAAAAAA"
-    assert OsmShortlink.osm_shortlink(180, -90, 16, false) == "http://osm.org/go/YAAAAAAA"
-    assert OsmShortlink.osm_shortlink(-180, -90, 0, false) == "http://osm.org/go/YAA--"
-    assert OsmShortlink.osm_shortlink(-180, 90, 0, false) == "http://osm.org/go/4AA--"
-    assert OsmShortlink.osm_shortlink(180, 90, 0, false) == "http://osm.org/go/4AA--"
-    assert OsmShortlink.osm_shortlink(180, -90, 0, false) == "http://osm.org/go/YAA--"
+    assert OsmShortlink.generate_link(-90, -180, 0, false) == "http://osm.org/go/AAA--"
+    assert OsmShortlink.generate_link(90, -180, 0, false) == "http://osm.org/go/AAA--"
+    assert OsmShortlink.generate_link(90, 180, 0, false) == "http://osm.org/go/AAA--"
+    assert OsmShortlink.generate_link(-90, 180, 0, false) == "http://osm.org/go/AAA--"
+    assert OsmShortlink.generate_link(-180, -90, 16, false) == "http://osm.org/go/YAAAAAAA"
+    assert OsmShortlink.generate_link(-180, 90, 16, false) == "http://osm.org/go/4AAAAAAA"
+    assert OsmShortlink.generate_link(180, 90, 16, false) == "http://osm.org/go/4AAAAAAA"
+    assert OsmShortlink.generate_link(180, -90, 16, false) == "http://osm.org/go/YAAAAAAA"
+    assert OsmShortlink.generate_link(-180, -90, 0, false) == "http://osm.org/go/YAA--"
+    assert OsmShortlink.generate_link(-180, 90, 0, false) == "http://osm.org/go/4AA--"
+    assert OsmShortlink.generate_link(180, 90, 0, false) == "http://osm.org/go/4AA--"
+    assert OsmShortlink.generate_link(180, -90, 0, false) == "http://osm.org/go/YAA--"
   end
 
   test "bounds" do
-    assert OsmShortlink.osm_shortlink(0, 0, 0, false) == "http://osm.org/go/wAA--"
-    assert OsmShortlink.osm_shortlink(90, 0, 16, false) == "http://osm.org/go/gAAAAAAA"
-    assert OsmShortlink.osm_shortlink(-90, 0, 16, false) == "http://osm.org/go/gAAAAAAA"
-    assert OsmShortlink.osm_shortlink(0, 180, 16, false) == "http://osm.org/go/QAAAAAAA"
-    assert OsmShortlink.osm_shortlink(0, -180, 16, false) == "http://osm.org/go/QAAAAAAA"
-    assert OsmShortlink.osm_shortlink(0, 0, 16, false) == "http://osm.org/go/wAAAAAAA"
+    assert OsmShortlink.generate_link(0, 0, 0, false) == "http://osm.org/go/wAA--"
+    assert OsmShortlink.generate_link(90, 0, 16, false) == "http://osm.org/go/gAAAAAAA"
+    assert OsmShortlink.generate_link(-90, 0, 16, false) == "http://osm.org/go/gAAAAAAA"
+    assert OsmShortlink.generate_link(0, 180, 16, false) == "http://osm.org/go/QAAAAAAA"
+    assert OsmShortlink.generate_link(0, -180, 16, false) == "http://osm.org/go/QAAAAAAA"
+    assert OsmShortlink.generate_link(0, 0, 16, false) == "http://osm.org/go/wAAAAAAA"
   end
 end
